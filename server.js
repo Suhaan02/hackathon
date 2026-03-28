543f9ef9f8c619865ef6d4f359eb809252f93e14
developerwordz <gamingbluebot@gmail.com>
2026-03-28 12:13:12 +0530

    deploy test

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
+app.get("/", (req, res) => {
+  res.send("🚑 Ambulance backend is running");
+});
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
